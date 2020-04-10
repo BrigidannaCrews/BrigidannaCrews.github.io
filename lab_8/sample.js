cfc1c5cc88d43d330eae0458b18bcc3c6ea423e6
Brigidanna_Crews <brigidannaccrews@gmail.com>
2020-04-10 13:09:27 -0400

    created js, fixed html so it goes through all the objects

---
+++ lab_8/sample.js
@@ -0,0 +1,35 @@
+/*function callbackFnDemo(arg1, cb){
+    if(arg === 2){
+        cb = arg1 * 2;
+    }
+}
+
+callbackFnDemo(2, (x) =>{
+
+    callbackFnDemo(x, function(x){
+        callbackFnDemo(3, () => {
+            x
+        })
+    })
+
+})*/
+
+data.reduce((result, currentItem) => {
+
+    if(!result[currentItem.category]){
+        result[currentItem.cateogry] = [];
+    }
+
+    result[currentItem.category].push(currentItem);
+
+    return result;
+
+}, {});
+
+
+arr.map(m => {
+    return m.x;
+})
+
+.find
+.filter
