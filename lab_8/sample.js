a88be8fc3e883b4a1bab13fc5d015ff1db5c32ba
Brigidanna_Crews <brigidannaccrews@gmail.com>
2020-04-10 18:21:05 -0400

    js complete but needs debugging

--- lab_8/sample.js
+++ lab_8/sample.js
@@ -33,3 +33,13 @@ arr.map(m => {
 
 .find
 .filter
+
+function explodePie (e) {
+	if(typeof (e.dataSeries.dataPoints[e.dataPointIndex].exploded) === "undefined" || !e.dataSeries.dataPoints[e.dataPointIndex].exploded) {
+		e.dataSeries.dataPoints[e.dataPointIndex].exploded = true;
+	} else {
+		e.dataSeries.dataPoints[e.dataPointIndex].exploded = false;
+	}
+	e.chart.render();
+
+}
